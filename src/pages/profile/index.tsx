cfc622f628983bc13707f89c311419b29e43cd7f
Amy Fischer <amy.tl.fischer@gmail.com>
2022-10-29 22:04:12 -0700

    Minor typescript error fixes

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,15 @@
 import Card from '@app-src/common/components/layout/Card';
 import ProfileHeader from '@app-src/modules/profile/components/ProfileHeader';
+import { selectProfileAddress } from '@app-src/store/protocol/selectors';
+import { useCallback } from 'react';
+import { useSelector } from 'react-redux';
 
 const ProfilePage = () => {
+  const profileAddress = useSelector(useCallback(selectProfileAddress, []));
+
   return (
     <div className="flex flex-col gap-y-24">
-      <ProfileHeader />
+      <ProfileHeader address="profileAddress" />
       <div className="w-full flex flex-col items-center gap-y-30">
         <Card>
           <div className="flex flex-row justify-center">
